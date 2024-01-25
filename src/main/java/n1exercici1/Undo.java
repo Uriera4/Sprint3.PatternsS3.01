712e322ec421409ca57f9e6a635579450fb9b8f3
Oriol Riera Lizcano <152235615+Uriera4@users.noreply.github.com>
2024-01-25 22:18:12 +0100

    "n1exercici1"

--- src/main/java/n1exercici1/Undo.java
+++ src/main/java/n1exercici1/Undo.java
@@ -1,2 +1,31 @@
-package org.example;public class Undo {
+package n1exercici1;
+
+import java.util.ArrayList;
+
+public class Undo {
+
+    private static Undo instancia;
+    private final ArrayList<String> llistaComandes;
+
+    private Undo (){
+        llistaComandes = new ArrayList<>();
+    }
+    public static Undo getInstancia(){
+        if (instancia == null) instancia = new Undo();
+        return instancia;
+    }
+
+    public void afegirComanda (String comanda){
+        llistaComandes.add(comanda);
+    }
+    public void impreixComandes (){
+        for (String comanda : llistaComandes){
+            System.out.println(comanda);
+        }
+        System.out.println();
+    }
+    public void eliminaComanda (){
+        if (!llistaComandes.isEmpty()) llistaComandes.removeLast();
+    }
+
 }
