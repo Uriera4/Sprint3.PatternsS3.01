56a6fe51e85f426540dba3e6507f44bc198e535a
urier <orarpione@gmail.com>
2024-01-25 22:16:12 +0100

    n1exercici1

---
+++ src/main/java/n1exercici1/ExcepcioOpcioNoValida.java
@@ -0,0 +1,8 @@
+package n3exercici1;
+
+public class ExcepcioOpcioNoValida extends Exception {
+
+    public ExcepcioOpcioNoValida (String message){
+        super(message);
+    }
+}
---
+++ src/main/java/n1exercici1/NomesSioNoException.java
@@ -0,0 +1,8 @@
+package n3exercici1;
+
+public class NomesSioNoException extends Exception{
+
+    public NomesSioNoException (String missatge){
+        super (missatge);
+    }
+}
